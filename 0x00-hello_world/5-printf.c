ae157ed33a3662b60db5fb39b39080288c6eb21d
wahinyak <wahinyakinaro@gmail.com>
2023-10-05 16:30:18 -0700

    added my 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include<stdio.h>
+/*
+ * main - Entry Point
+ *
+ * return : Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
